5a946d6f1f9c7c1559339324bc80c6cc72de3f03
fikriwado <fixwad.online@gmail.com>
2021-06-15 05:51:22 +0700

    add smothscroll

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 $(function () {
     AOS.init({
-        disable: function() {
+        disable: function () {
             var maxWidth = 992;
             return window.innerWidth < maxWidth;
         }
     });
-    
+
     $('.owl-carousel').owlCarousel({
-        items:4,
+        items: 4,
         loop: true,
         margin: 20,
-        autoWidth:true,
+        autoWidth: true,
         dots: false
     });
-});
 
+    $('.menu-items-scroll').on('click', function (e) {
+        e.preventDefault();
+        let thisMenu = $(this).attr('href');
+        let target = $(thisMenu);
+        $('html, body').animate({
+            scrollTop: target.offset().top - 250
+        }, 1000, 'easeInOutExpo');
+    });
+});
